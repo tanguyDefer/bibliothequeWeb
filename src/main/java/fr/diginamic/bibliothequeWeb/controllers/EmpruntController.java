081a2c9c96c8eacd5cf3dffcda91c64452f873ad
tanguyD <tanguy59192@hotmail.fr>
2021-10-21 14:01:04 +0200

    [TP-bibliotheque web] Refacto

--- src/main/java/fr/diginamic/bibliothequeWeb/controllers/EmpruntController.java
+++ src/main/java/fr/diginamic/bibliothequeWeb/controllers/EmpruntController.java
@@ -1,16 +1,20 @@
 package fr.diginamic.bibliothequeWeb.controllers;
 
+import fr.diginamic.bibliothequeWeb.entities.Client;
 import fr.diginamic.bibliothequeWeb.entities.Emprunt;
 import fr.diginamic.bibliothequeWeb.entities.Livre;
+import fr.diginamic.bibliothequeWeb.exceptions.ClientError;
 import fr.diginamic.bibliothequeWeb.exceptions.EmpruntError;
 import fr.diginamic.bibliothequeWeb.repository.ClientRepository;
 import fr.diginamic.bibliothequeWeb.repository.EmpruntRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
@@ -49,13 +53,25 @@ public class EmpruntController {
     }
 
 
-    @PutMapping("update/{id}")
-    public String empruntUpdateOne(@PathVariable("id") Integer id,
-                                   @Valid @RequestBody Emprunt pe) {
-        Emprunt e = (Emprunt) empruntRepository.findById(id).get();
+    @GetMapping("/update/{id}")
+    public String updateEmprunt(Model model, @PathVariable("id") Integer id) {
+        model.addAttribute("empruntForm", new Emprunt());
+        model.addAttribute("titre", "Modification de l'emprunt");
+        model.addAttribute("emprunt", empruntRepository.findById(id).get());
+        return "emprunts/updateEmprunt";
+    }
 
-        empruntRepository.save(pe);
-        return "Update Ok : " + e.getId();
+
+    @PostMapping("/update/{id}")
+    public String updateClient(@PathVariable("id") Integer id,
+                               @Valid @ModelAttribute("empruntForm") Emprunt empruntForm) throws EmpruntError {
+        Optional<Emprunt> e = empruntRepository.findById(id);
+        if (e.isEmpty()) {
+            throw (new EmpruntError("Emprunt id :" + id + " non trouvé : impossible pour faire un update"));
+        }
+        System.err.println(empruntForm);
+        empruntRepository.save(empruntForm);
+        return "redirect:/client/getEmprunt/{id}";
     }
 
     @PostMapping("create")
